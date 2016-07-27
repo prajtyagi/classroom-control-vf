e776d16c22ac4e8c21cb0c77c690b1c89a599292
prajtyagi <prajtyagi@taskstream.com>
2016-07-27 15:46:23 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -9,6 +9,12 @@ class nginx {
     require => Package['nginx'],
     }
   
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => present,
+    source => puppet:///modules/nginx/default.conf
+    require => Package['nginx'],
+    }
+  
   service {'nginx':
     ensure => running,
     enable => true,
