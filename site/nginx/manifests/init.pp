f5892e6489c11f4e92b9b53ea0171d214cd4a1ff
prajtyagi <prajtyagi@taskstream.com>
2016-07-27 15:43:36 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,5 +1,17 @@
 class nginx {
-  file { '/etc/
-
-
+  package { 'nginx':
+    ensure => present,
+    }
+    
+  file { '/etc/nginx/nginx.conf':
+    ensure => present,
+    source => puppet:
+    require => Package['nginx'],
+    }
+  
+  service {'memcached':
+    ensure => running,
+    enable => true,
+    subscribe => File['/etc/sysconfig/memcached']
+  }
 }
