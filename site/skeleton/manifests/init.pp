6a514220f87977a936066a6d78f227f220183484
prajtyagi <prajtyagi@taskstream.com>
2016-07-27 11:27:17 -0400

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,7 +1,9 @@
-file { '/etc/skel/.bashrc':
+class skeleton {
+  file { '/etc/skel/.bashrc':
    ensure  => file,
    owner   => 'root',
    group   => 'root',
    mode    => '0644',
    sources  => 'puppet:///site/skeleton/files/bashrc',
- }
+   }
+}
