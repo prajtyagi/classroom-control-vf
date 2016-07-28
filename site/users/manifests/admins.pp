696b49b1882ed9f260a3095d320bcc12d926a3b5
prajtyagi <prajtyagi@taskstream.com>
2016-07-28 12:58:49 -0400

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,5 +1,7 @@
 class users::admins {
-  users::managed_user { 'jose': group => admin,}
+  users::managed_user { 'jose': 
+      group => admin,
+      }
   users::managed_user { 'alice': }
   users::managed_user { 'chen' :
       group => admin,
