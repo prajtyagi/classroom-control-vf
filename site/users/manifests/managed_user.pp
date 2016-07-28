edadb362bc86ad84be5f4b1e71023b8edd2a52a7
prajtyagi <prajtyagi@taskstream.com>
2016-07-28 12:31:58 -0400

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,5 +1,18 @@
-define users::mananged_user {
+define users::managed_user (
   $group = $title,
-  
-
-}
+  ) {
+  user { $title:
+    ensure => present,
+  }
+  file { "/home/${title}":
+    ensure => directory,
+    owner  => $title,
+    group  => $group,
+  }
+  file { "/home/${title}/.ssh":
+    ensure => directory,
+    owner  => $title,
+    group  => $group,
+    mode   => '0600',
+  }
+}  
