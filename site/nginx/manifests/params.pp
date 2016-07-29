c9defaa10c6bf6125d22c1297bca00044d5eee41
prajtyagi <prajtyagi@taskstream.com>
2016-07-29 11:59:13 -0400

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,28 +1,46 @@
 class nginx::params {
-
   case $::osfamily {
-
-    'Debian': {
-       'packagename' => 'nginx',
-       'fileowner'   => 'root',
-       'filegroup'   => 'root',
-       'docroot'     => '/var/www',
-       'configdir'   => '/etc/nginx',
-       'serverbdir'  => '/etc/nginx/conf.d',
-       'logsdir'     => '/var/log/nginx',
-       'servicename' => 'nginx',
-       'servicerunas'=> 'www-data',
-      }
     'RedHat': {
-        'packagename' => 'nginx',
-       'fileowner'   => 'root',
-       'filegroup'   => 'root',
-       'docroot'     => '/var/www',
-       'configdir'   => '/etc/nginx',
-       'serverbdir'  => '/etc/nginx/conf.d',
-       'logsdir'     => '/var/log/nginx',
-       'servicename' => 'nginx',
-       'servicerunas'=> 'nginx',
-      }
+      $port = 80
+      $pkg = 'nginx'
+      $etc_dir = '/etc/nginx'
+      $fowner = 'root'
+      $fgroup = 'root'
+#      $docroot = '/var/www'
+      $full_docroot = '/var/www'
+      $server_block = "${etc_dir}/conf.d"
+      $log_dir = '/var/log/nginx'
+      $service = 'nginx'
+      $run_as  = 'nginx'
     }
+    'Debian': {
+      $port = 80
+      $pkg = 'nginx'
+      $etc_dir = '/etc/nginx'
+      $fowner = 'root'
+      $fgroup = 'root'
+#      $docroot = '/var/www'
+      $full_docroot = '/var/www'
+      $server_block = "${etc_dir}/conf.d"
+      $log_dir = '/var/log/nginx'
+      $service = 'nginx'
+      $run_as  = 'www-data'
+    }
+    'windows': {
+      $port = 80
+      $pkg = 'nginx-service'
+      $etc_dir = 'C:/ProgramData/nginx'
+      $fowner = 'Administrator'
+      $fgroup = 'Administrators'
+#      $docroot = 'C:/ProgramData/nginx/html'
+      $full_docroot = 'C:/ProgramData/nginx/html'
+      $server_block = "${etc_dir}/conf.d"
+      $log_dir = 'C:/ProgramData/gninx/logs'
+      $service = 'nginx'
+      $run_as  = 'nobody'
+    }
+    default: {
+      fail("Operating system #{operatingsystem} is not supported.")
+    }
+  }
 }
