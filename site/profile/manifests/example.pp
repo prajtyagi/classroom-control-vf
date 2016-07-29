096028c1853e303d2e643daa3c47a55cc916eb6b
prajtyagi <prajtyagi@taskstream.com>
2016-07-29 16:08:17 -0400

    Update example.pp

--- site/profile/manifests/example.pp
+++ site/profile/manifests/example.pp
@@ -1,12 +1,16 @@
 class profile::example {
   include apache
-  include wordpress
+
+class { 'wordpress':
+    install_dir => '/var/www/html',
+  }
+  
   class { '::mysql::server':
   root_password           => 'strongpassword',
 }
+
  class { 'mysql::bindings':
         php_enable => true,
   }
-  
   notify { 'This is the example profile!': }
 }
