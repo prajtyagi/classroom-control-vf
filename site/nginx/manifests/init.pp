49b3aa63ce527ffadb19928ad5af7c7d73cfba1e
prajtyagi <prajtyagi@taskstream.com>
2016-07-29 11:58:11 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,49 +2,7 @@ class nginx (
   $root = undef,
   ) {
 
-  case $::osfamily {
-    'RedHat': {
-      $port = 80
-      $pkg = 'nginx'
-      $etc_dir = '/etc/nginx'
-      $fowner = 'root'
-      $fgroup = 'root'
-#      $docroot = '/var/www'
-      $full_docroot = '/var/www'
-      $server_block = "${etc_dir}/conf.d"
-      $log_dir = '/var/log/nginx'
-      $service = 'nginx'
-      $run_as  = 'nginx'
-    }
-    'Debian': {
-      $port = 80
-      $pkg = 'nginx'
-      $etc_dir = '/etc/nginx'
-      $fowner = 'root'
-      $fgroup = 'root'
-#      $docroot = '/var/www'
-      $full_docroot = '/var/www'
-      $server_block = "${etc_dir}/conf.d"
-      $log_dir = '/var/log/nginx'
-      $service = 'nginx'
-      $run_as  = 'www-data'
-    }
-    'windows': {
-      $port = 80
-      $pkg = 'nginx-service'
-      $etc_dir = 'C:/ProgramData/nginx'
-      $fowner = 'Administrator'
-      $fgroup = 'Administrators'
-#      $docroot = 'C:/ProgramData/nginx/html'
-      $full_docroot = 'C:/ProgramData/nginx/html'
-      $server_block = "${etc_dir}/conf.d"
-      $log_dir = 'C:/ProgramData/gninx/logs'
-      $service = 'nginx'
-      $run_as  = 'nobody'
-    }
-    default: {
-      fail("Operating system #{operatingsystem} is not supported.")
-    }
+  
   }
  
   $docroot = $root ? {
